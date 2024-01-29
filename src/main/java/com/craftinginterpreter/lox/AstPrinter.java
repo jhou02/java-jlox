94d5da7d2c77b8be10d462f22890139011759f88
Jonathan Hou <jhou03@email.wm.edu>
2024-01-28 21:18:02 -0500

    Chapter 5 progress

---
+++ src/main/java/com/craftinginterpreter/lox/AstPrinter.java
@@ -0,0 +1,32 @@
+package com.craftinginterpreter.lox;
+
+class AstPrinter implements Expr.Visitor<String> {
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme,
+                expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) {
+            return "nil";
+        }
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+}
