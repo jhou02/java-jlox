c910a9987e7b8252235521bc44c26dac7ad03646
Jonathan Hou <jhou03@email.wm.edu>
2024-01-30 22:06:36 -0500

    Start Parser.java

---
+++ src/main/java/com/craftinginterpreter/lox/Parser.java
@@ -0,0 +1,15 @@
+package com.craftinginterpreter.lox;
+
+import java.util.List;
+
+import static com.craftinginterpreter.lox.TokenType.*;
+
+class Parser {
+
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+}
